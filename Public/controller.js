6066ce5b4dbb1d1922a025bdfd80ad0ae754a153
joe-broder15 <joey.broder@gmail.com>
2017-12-20 11:43:58 -0800

    CONVERTED TO ALEXA SKILL BUILDER

---
+++ Public/controller.js
@@ -0,0 +1,20 @@
+var app = angular.module('app', []);
+
+app.controller('templateCtrl', function($scope) {
+    $scope.template = "Select";
+    $scope.powerInfo = "";
+    $scope.change_template = function(t) {
+      $scope.template = t;
+      if(t == "Alexa Flash Briefing"){
+        $scope.powerInfo = "Create an Alexa flash briefing with a custom feed";
+      } else if (t == "Alexa Interaction") {
+        $scope.powerInfo = "Create a custom interaction for your Alexa";
+      };
+    };
+    $scope.next_card = function(){
+
+      $( "#appcontainer" ).load( "Cards/testCard.html");
+
+    };
+
+});
